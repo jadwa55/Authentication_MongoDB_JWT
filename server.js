737ac13f0385564ae9aa3ca28d01a0050f797bbb
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-01-13 19:30:36 +0100

    import express & cors modules & create an Express app

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require ("express");
+const cors = require ("cors");  //cors provides Express middleware to enable CORS
+
+const app = express();
+
+var corsOption = {
+    origin: "http://localhost:8081"
+};
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Hell AgaiiiiiiiiiiiiN." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
