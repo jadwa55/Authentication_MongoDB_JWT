d11f4ed4e9eb4acb6d1a9569becfb2012ec4f8dd
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-01-17 11:48:55 +0100

    Require Routes in server.js

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to Hell AgaiiiiiiiiiiiiN." });
 });
 
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
